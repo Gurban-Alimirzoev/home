a80d6e79a50482a80ce0ea50695e96959de4f4a4
Gurban-Alimirzoev <79535752797@yandex.ru>
2021-12-10 21:35:28 +0300

    fix

--- c++/c_emaple.cpp
+++ c++/c_emaple.cpp
@@ -7,51 +7,50 @@
 
 using namespace std;
 
-const int sample_count = 5;
+const int SAMPLE_COUNT = 5;
 
-vector<int> countnameslong(const set<string>& storage, const vector<string>& candidates) {
-	vector<int> output;
-	for (auto& name : candidates) {
-		output.push_back(count(storage.begin(), storage.end(), name));
-	}
-	return output;
+vector<int> CountNamesLong(const set<string>& storage, const vector<string>& candidates) {
+    vector<int> output;
+    for (auto& name: candidates) {
+        output.push_back(count(storage.begin(), storage.end(), name));
+    }
+    return output;
 }
 
-vector<int> countnames(const set<string>& storage, const vector<string>& candidates) {
-	vector<int> ret;
-	for (const auto i : candidates) {
-		if (storage.count(i)) {
-			ret.push_back(1);
-		}
-		else {
-			ret.push_back(0);
-		}
-	}
-	return ret;
+vector<int> CountNames (const set<string>& storage, const vector<string>& candidates) {
+    vector<int> ret;
+    for (const auto i : candidates) {
+        if (storage.count(i)) {
+        ret.push_back(1);
+        }
+        else {
+        ret.push_back(0);
+        }
+    }
+    return ret;
 }
-
+    
 int main() {
-	set<string> s;
-	vector<string> v;
-	string stra = ""s;
-	string strb = ""s;
+    set<string> s;
+    vector<string> v;
+    string stra = ""s;
+    string strb = ""s;
 
-	for (int j = 0; j < 10000; ++j) {
-		s.insert(stra);
-		stra += "a"s;
-		if (j % 2 == 0) {
-			v.push_back(strb);
-			strb += "b"s;
-		}
-		else {
-			v.push_back(stra);
-		}
-	}
-	cout << "Testing slow version" << endl;
-	for (int i = 0; i < SAMPLE_COUNT; ++i) {
-		auto begin = chrono::steady_clock::now();
-		CountNames(s, v);
-		auto end = chrono::steady_clock::now();
-		cout << "Time difference Long = "s << chrono::duration_cast<chrono::microseconds>(end - begin).count() << "[µs]"s << endl;
-	}
+    for (int j = 0; j < 10000; ++j) {
+        s.insert(stra);
+        stra += "a"s;
+        if (j % 2 == 0) {
+            v.push_back(strb);
+            strb += "b"s;
+        } else {
+            v.push_back(stra);
+        }
+    }
+    cout << "Testing slow version" << endl;
+    for (int i = 0; i < SAMPLE_COUNT; ++i) {
+        auto begin = chrono::steady_clock::now();
+        CountNames(s, v);
+        auto end = chrono::steady_clock::now();
+        cout << "Time difference Long = "s << chrono::duration_cast<chrono::microseconds>(end - begin).count() << "[µs]"s << endl;
+    }
 }
