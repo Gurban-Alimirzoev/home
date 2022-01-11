9efaef41ac3fcbad51a4bcf5593fe807c12a835b
Gurban-Alimirzoev <79535752797@yandex.ru>
2022-01-11 20:14:33 +0300

    Create palindrome.cpp

---
+++ c++/palindrome.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool IsPalindrome(const string& text) {
+    int size_text = text.size(), var = 0;
+    string prob;
+    for (int i = 0; i < size_text; i++) {
+        if (text[i] != ' ') {
+            prob.push_back(text[i]);
+        }
+    }
+    if (prob.empty()) {
+        return false;
+    }
+    else {
+        int size_prob = prob.size();        
+        for (int i = 0; i < (size_prob / 2); i++) {
+            if (prob[i] == prob[size_text - i - 1]) {
+                var++;
+            }
+            else {
+                break;
+                }  
+            }
+        if (var == size_prob / 2)  {
+            return true;
+            }
+        else {
+            return false;
+            }
+        }
+    }
+
+int main() {
+    string text = "never odd or even";
+
+    if (IsPalindrome(text)) {
+        cout << "palindrome"s << endl;
+    } else {
+        cout << "not a palindrome"s << endl;
+    }
+}
