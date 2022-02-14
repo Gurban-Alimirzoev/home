e9b0c1289c953130abc18f719b436137bfcfd28f
Gurban-Alimirzoev <79535752797@yandex.ru>
2022-02-14 23:47:50 +0300

    fix

--- c++/old_example.cpp
+++ c++/old_example.cpp
@@ -1,30 +1,27 @@
+#include <vector>
+#include <numeric>
+#include <iterator>
 
-    Wall::Wall(double width, double height) 
-    : width_(width), height_(height), color_(Color::WHITE) 
-{
-}
+using namespace std;
 
-enum class Color {
-        BLUE,
-        GREEN,
-        RED,
-        WHITE,
-        YELLOW
-    };
-    
-double Wall::GetHeight() const {
-    return height_;
-} 
-double Wall::GetWidth() const {
-    return width_;
-} 
-void Wall::SetColor(Color color) {
-    color_ = color;
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double average = accumulate(nums.begin(), next(nums.begin(), k), 0.0);
+        double result = average, newAve;
+        for (auto i = nums.begin(); i < (nums.end() - k); i++) {
+            newAve = average - *i + *(i + k);
+            if (result < newAve) {
+                result = newAve;
+            }
+            average = newAve; 
+        }
+        return result/k;
+    }
+};
+
+int main() {
+    std::vector<int> vec = {1,12,-5,-6,50,3};
+    Solution Sol;
+    Sol.findMaxAverage(vec, 4);
 }
-Color Wall::GetColor() const {
-    return color_
-}    
-private:
-    double width_;
-    double height_;
-    Color color_;    
