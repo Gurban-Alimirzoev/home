493950d0e6bc2b0a0f0b5b5102231d4f4d0ed526
Gurban Alimirzoev <95102129+Gurban-Alimirzoev@users.noreply.github.com>
2022-03-28 15:51:14 +0300

    fix

--- c++/SimpleVector/simple_vector.h
+++ c++/SimpleVector/simple_vector.h
@@ -3,7 +3,9 @@
 #include <cassert>
 #include <initializer_list>
 #include <array>
-#include "array_ptr.cpp"
+#include <stdexcept>
+#include <algorithm>
+#include "array_ptr.h"
 
 using namespace std;
 
@@ -15,137 +17,175 @@ public:
     using ConstIterator = const Type *;
 
     SimpleVector() noexcept = default;
+    ~SimpleVector()
+    {
+        delete data_;
+    }
 
     // Создаёт вектор из size элементов, инициализированных значением по умолчанию
-    explicit SimpleVector(size_t size) : size_(size), capacity(size)
+    explicit SimpleVector(size_t size)
+        : size_(size), capacity_(size)
     {
-        Type var[size_] = {NULL};
-        ArrayPtr<Type> arr_vec(*var);
+        data_ = new ArrayPtr<Type>(size);
+        fill(begin(), end(), Type());
     }
 
     // Создаёт вектор из size элементов, инициализированных значением value
-    SimpleVector(size_t size, const Type &value) : size_(size), capacity(size)
+    SimpleVector(size_t size, const Type &value)
+        : size_(size), capacity_(size)
     {
-        Type var[size_] = {value};
-        ArrayPtr<Type> arr_vec(*var);
+        data_ = new ArrayPtr<Type>(size);
+        fill(begin(), end(), value);
     }
 
     // Создаёт вектор из std::initializer_list
-    SimpleVector(std::initializer_list<Type> init) : size_(init.size()), capacity(init.size())
+    SimpleVector(std::initializer_list<Type> init)
+        : size_(init.size()), capacity_(init.size())
     {
-        Type var[size_] = {NULL};
-        copy(init.begin(), init.end(), var);
-        ArrayPtr<Type> arr_vec(*var);
+
+        data_ = new ArrayPtr<Type>(size_);
+        std::copy(init.begin(), init.end(), begin());
+    }
+
+    SimpleVector &operator=(const SimpleVector &rhs)
+    {
+        assert(*this != rhs);
+        SimpleVector<Type> rhs_copy(rhs);
+        swap(rhs_copy);
+        return *this;
     }
 
     // Возвращает количество элементов в массиве
     size_t GetSize() const noexcept
     {
-        // Напишите тело самостоятельно
         return size_;
     }
 
     // Возвращает вместимость массива
     size_t GetCapacity() const noexcept
     {
-        // Напишите тело самостоятельно
-        return capacity;
+        return capacity_;
     }
 
     // Сообщает, пустой ли массив
     bool IsEmpty() const noexcept
     {
-        return true;
+        return size_ == 0u;
     }
 
     // Возвращает ссылку на элемент с индексом index
     Type &operator[](size_t index) noexcept
     {
-        return arr_vec[index];
+        return *(data_->Get() + index);
     }
-
     // Возвращает константную ссылку на элемент с индексом index
     const Type &operator[](size_t index) const noexcept
     {
-        // Напишите тело самостоятельно
-        return arr_vec[index];
+        return *(data_->Get() + index);
     }
 
     // Возвращает константную ссылку на элемент с индексом index
     // Выбрасывает исключение std::out_of_range, если index >= size
-    Type &At(size_t index)
+    const Type &At(size_t index)
     {
-        // Напишите тело самостоятельно
-        return arr_vec[index];
+        if (!(index >= size_))
+        {
+            return *(data_->Get() + index);
+        }
+        else
+        {
+            throw out_of_range("Exception: Index out of range!");
+        }
+        return *data_->Get();
     }
 
     // Возвращает константную ссылку на элемент с индексом index
     // Выбрасывает исключение std::out_of_range, если index >= size
     const Type &At(size_t index) const
     {
-        // Напишите тело самостоятельно
-        return arr_vec[index];
+        if (!(index >= size_))
+        {
+            return *(data_->Get() + index);
+        }
+        else
+        {
+            throw out_of_range("Exception: Index out of range!");
+        }
+        return *data_->Get();
     }
 
     // Обнуляет размер массива, не изменяя его вместимость
     void Clear() noexcept
     {
-        size_ = 0u;
-        // Напишите тело самостоятельно
+        size_ = 0;
     }
 
     // Изменяет размер массива.
     // При увеличении размера новые элементы получают значение по умолчанию для типа Type
     void Resize(size_t new_size)
     {
-        // Напишите тело самостоятельно
+        if (new_size > size_)
+        {
+            auto new_array = new ArrayPtr<Type>(new_size);
+
+            fill(new_array->Get(), new_array->Get() + new_size, Type());
+            copy(begin(), begin() + size_, new_array->Get());
+
+            data_ = new_array;
+
+            size_ = new_size;
+            capacity_ = new_size;
+        }
+        else
+        {
+            size_ = new_size;
+        }
     }
 
     // Возвращает итератор на начало массива
     // Для пустого массива может быть равен (или не равен) nullptr
     Iterator begin() noexcept
     {
-        return *arr_vec.Get();
-        // Напишите тело самостоятельно
+        return (size_ == 0) ? Iterator() : Iterator(data_->Get());
     }
 
     // Возвращает итератор на элемент, следующий за последним
     // Для пустого массива может быть равен (или не равен) nullptr
     Iterator end() noexcept
     {
-        // Напишите тело самостоятельно
+        return (size_ == 0) ? Iterator() : Iterator(data_->Get() + size_);
     }
 
     // Возвращает константный итератор на начало массива
     // Для пустого массива может быть равен (или не равен) nullptr
     ConstIterator begin() const noexcept
     {
-        // Напишите тело самостоятельно
+        return (size_ == 0) ? ConstIterator() : ConstIterator(data_->Get());
     }
 
     // Возвращает итератор на элемент, следующий за последним
     // Для пустого массива может быть равен (или не равен) nullptr
     ConstIterator end() const noexcept
     {
-        // Напишите тело самостоятельно
+        return (size_ == 0) ? ConstIterator() : ConstIterator(data_->Get() + size_);
     }
 
     // Возвращает константный итератор на начало массива
     // Для пустого массива может быть равен (или не равен) nullptr
     ConstIterator cbegin() const noexcept
     {
-        // Напишите тело самостоятельно
+        return (size_ == 0) ? ConstIterator() : ConstIterator(data_->Get());
     }
 
     // Возвращает итератор на элемент, следующий за последним
     // Для пустого массива может быть равен (или не равен) nullptr
     ConstIterator cend() const noexcept
     {
-        // Напишите тело самостоятельно
+        return (size_ == 0) ? ConstIterator() : ConstIterator(data_->Get() + size_);
     }
 
 private:
-    size_t size_;
-    size_t capacity;
-    ArrayPtr<Type> arr_vec();
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+    ArrayPtr<Type> *data_ = nullptr;
 };
