23f72bc154ad983bff10ad69fcc93e9306602997
Gurban Alimirzoev <95102129+Gurban-Alimirzoev@users.noreply.github.com>
2022-04-18 18:46:24 +0300

    fix

--- c++/old_example.cpp
+++ c++/old_example.cpp
@@ -1,89 +1,93 @@
-#include <functional>
-#include <iostream>
-#include <map>
-#include <set>
-#include <deque>
-#include <list>
-#include <sstream>
-#include <string>
-#include <future>
+#include <algorithm>
 #include <execution>
-#include <utility>
+#include <iostream>
+#include <list>
+#include <random>
+#include <string>
 #include <string_view>
+#include <type_traits>
+#include <vector>
+#include <future>
+
+#include "log_duration.h"
+
 using namespace std;
 
-using KeyWords = set<string, less<>>;
+string GenerateWord(mt19937& generator, int max_length) {
+    const int length = uniform_int_distribution(1, max_length)(generator);
+    string word;
+    word.reserve(length);
+    for (int i = 0; i < length; ++i) {
+        word.push_back(uniform_int_distribution('a', 'z')(generator));
+    }
+    return word;
+}
 
-struct Stats
-{
-    map<string, int> word_frequences;
+template <template <typename> typename Container>
+Container<string> GenerateDictionary(mt19937& generator, int word_count, int max_length) {
+    vector<string> words;
+    words.reserve(word_count);
+    for (int i = 0; i < word_count; ++i) {
+        words.push_back(GenerateWord(generator, max_length));
+    }
+    return Container(words.begin(), words.end());
+}
 
-    Stats &operator+=(const Stats &other)
-    {
-        for (const auto &p : other.word_frequences)
-        {
-            word_frequences[p.first] += p.second;
-        }
-        return *this;
+struct Reverser {
+    void operator()(string& value) const {
+        reverse(value.begin(), value.end());
     }
 };
 
-Stats GetStatsWords(const KeyWords &key_words, const string str)
-{
-    Stats result;
-    stringstream streamData(str);
-    string word;
-    list<string> vec_text;
-    while (getline(streamData, word, ' '))
-    {
-        vec_text.push_front(move(word));
-    }
+template <typename Container, typename Function>
+void Test(string_view mark, Container keys, Function function) {
+    LOG_DURATION(mark);
+    function(keys, Reverser{});
+}
 
-    for (auto &text : vec_text)
-    {
-        if (key_words.count(text) != 0u)
-        {
-            result.word_frequences[text] += 1;
+#define TEST(function) Test(#function, keys, function<remove_const_t<decltype(keys)>, Reverser>)
+
+template <typename ForwardRange, typename Function>
+void ForEach(ForwardRange& range, Function function) {
+    // ускорьте эту реализацию
+    //for_each(execution::par, range.begin(), range.end(), function);
+    const int threads = 2;    // количество задач/потоков
+    auto count_elements = range.size(); // количество элементов в исходном контейнере
+    // размер части массива для одной асинхронной задачи
+    // если количество элементов не кратно количеству потоков то увеличиваем на 1 чтобы было кратное число
+    size_t count_tasks = count_elements / threads + (count_elements % threads > 0 ? 1 : 0);
+    auto begin_range = range.begin();   // начало части массива
+    auto end_range = range.begin();     // конец части массива
+    auto mid = next(begin_range, count_tasks);
+
+    auto left_task = [function](auto iterF, auto iterL) {
+        for (; iterF != iterL; iterF++) {
+            function(*iterF);
         }
-    }
-    return result;
+    };
+
+    auto left_future = async([&function, left_task, begin_range, mid] { left_task(begin_range, mid); });
+    auto right_future = async([&function, left_task, mid, end_range] { left_task(mid, end_range); });
 }
 
-using KeyWords = set<string, less<>>;
+int main() {
+    // для итераторов с произвольным доступом тоже должно работать
+    vector<string> strings = { "cat", "dog", "code" };
 
-Stats ExploreKeyWords(const KeyWords &key_words, istream &input)
-{
-    list<future<Stats>> vec_stat;
-    //vec_stat.reserve(50);
-    Stats result;
-    string text;
-    while (input)
-    {
-        getline(input, text);
-        vec_stat.push_front(async(GetStatsWords, cref(key_words), move(text)));
+    ForEach(strings, [](string& s) {
+        reverse(s.begin(), s.end());
+        });
+
+    for (string_view s : strings) {
+        cout << s << " ";
     }
-    for (auto &vec : vec_stat)
-    {
-        result += vec.get();
-    }
-    return result;
-}
+    cout << endl;
+    // вывод: tac god edoc
 
-int main()
-{
-    const KeyWords key_words = {"yangle", "rocks", "sucks", "all"};
+    mt19937 generator;
+    const auto keys = GenerateDictionary<list>(generator, 50'000, 5'000);
 
-    stringstream ss;
-    ss << "this new yangle service really rocks\n";
-    ss << "It sucks when yangle isn't available\n";
-    ss << "10 reasons why yangle is the best IT company\n";
-    ss << "yangle rocks others suck\n";
-    ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
-
-    for (const auto &[word, frequency] : ExploreKeyWords(key_words, ss).word_frequences)
-    {
-        cout << word << " " << frequency << endl;
-    }
+    TEST(ForEach);
 
     return 0;
 }
