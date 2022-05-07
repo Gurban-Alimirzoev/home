f62becb7e9f36550f7222998cec0d2d2bac5cea8
Gurban-Alimirzoev <79535752797@yandex.ru>
2022-05-07 13:14:52 +0300

    Update ini.cpp

--- c++/ini.cpp
+++ c++/ini.cpp
@@ -6,17 +6,19 @@ namespace ini {
 
 Section& Document::AddSection(string name)
 {
-    
+    sections_.insert(name, {});
+    return sections_[name];
 }
     
 const Section& Document::GetSection(const string& name) const
 {
-    if (sections_.contains(name))
+    //unordered_map<string, Section> zero;
+    static const Section n;
+    if (sections_.at(name) != n)
     {
-        return sections_[name];
+        return sections_.at(name);
     }
-    unordered_map<string, Section> zero;
-    return zero;
+    return n;
 }
     
 size_t Document::GetSectionCount() const
@@ -26,10 +28,34 @@ size_t Document::GetSectionCount() const
     
 Document Load(std::istream& input)
 {
-    string str;
-    getline(input, str);
-    
-    
+    //string str;
+    //getline(input, str);
+    //string sec_name;
+    Section invis;
+    Section& sec_ref = invis;
+    Document doc;
+    for (string line; getline(input, line) && line[1] != '/';) 
+    {
+        auto pos_sec = line.find_first_not_of('[');
+        if (pos_sec != string::npos)
+        {
+            string sec_name = line.substr(pos_sec, line.find_last_not_of(']'));
+            sec_ref = doc.Document::AddSection(sec_name);
+            continue;
+        }        
+        line.erase(remove_if(line.begin(), 
+                              line.end(),
+                              [](auto x){return isspace(x);}),
+               line.end());
+        
+        auto pos_key = line.find('=');
+        
+        auto key_name = line.substr(0, pos_key - 1);
+        auto value = line.substr(pos_key + 1, line.size() - 1);
+
+        sec_ref = {key_name, value};     
+    }
+    return doc;
 }
 }
 // место для реализаций методов и функций библиотеки ini
