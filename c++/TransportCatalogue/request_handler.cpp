5af0ef2f4b43d438908e68dcbfd3214a6b102623
Gurban-Alimirzoev <95102129+Gurban-Alimirzoev@users.noreply.github.com>
2022-08-16 22:33:54 +0300

    Update request_handler.cpp

--- c++/TransportCatalogue/request_handler.cpp
+++ c++/TransportCatalogue/request_handler.cpp
@@ -7,3 +7,20 @@
  * Если вы затрудняетесь выбрать, что можно было бы поместить в этот файл,
  * можете оставить его пустым.
  */
+
+using namespace std;
+
+namespace transport_catalogue
+{
+
+	optional<BusInfo> RequestHandler::GetBusStat(const string_view& bus_name) const
+	{
+
+	}
+
+	const unordered_set<string> RequestHandler::GetBusesByStop(const string_view& stop_name) const
+	{
+
+	}
+
+}
