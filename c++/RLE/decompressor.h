716ef2422e770aba9f3f4879d41c3e6c7138f5f0
Gurban-Alimirzoev <95102129+Gurban-Alimirzoev@users.noreply.github.com>
2022-09-05 23:25:52 +0300

    fix

--- c++/RLE/decompressor.h
+++ c++/RLE/decompressor.h
@@ -1,9 +1,38 @@
 #pragma once
-
+#include <fstream>
+#include <iostream>
 #include <string>
 
 // напишите эту функцию
 inline bool DecodeRLE(const std::string& src_name, const std::string& dst_name)
 {
+    std::ifstream input(src_name, std::ios::binary);
+    if (!input) {
+        return false;
+    }
 
+    std::ofstream out(dst_name, std::ios::binary);
+    do
+    {
+        int c = input.get();
+        if (c == std::istream::traits_type::eof())
+            return true;
+
+        unsigned char header(c);
+        int block_type = (header & 1);
+        int data_size = (header >> 1) + 1;
+
+        if (block_type == 0) {
+            char buff[1024];
+            input.read(buff, data_size);
+            out.write(buff, data_size);
+        }
+        if (block_type == 1) {
+            const char c = input.get();
+            std::string str(data_size, c);
+            out << str;
+        }
+    }
+    while (input);
+    return true;
 }
--- c++/old_example.cpp
+++ c++/old_example.cpp
@@ -1,35 +1,88 @@
 #include <cassert>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 #include <string_view>
+#include <utility>
 
 using namespace std;
+using filesystem::path;
 
-// реализуйте эту функцию:
-size_t GetFileSize(string file)
+path operator""_p(const char* data, std::size_t sz) {
+    return path(data, data + sz);
+}
+
+bool operator>(const path& lhs,
+    const path& rhs)
 {
-    fstream fout(file, ios::in);
-    if (fout)
-    {
-        fout.seekg(0, ios::end);
-        return static_cast<size_t>(fout.tellg());
+    return lhs.filename().string() < rhs.filename().string();
+}
+
+void PrintTree(ostream& dst, const path& p, const filesystem::file_status& status, int offset)
+{
+    error_code err;
+    vector <path> result_file;
+    vector <path> result_dir;
+    if (err) {
+        return;
     }
-    else
+    dst << string(offset, ' ') << p.filename().string() << endl;
+    offset += 2;
+    for (const auto& dir_entry : filesystem::directory_iterator(p))
     {
-        return string::npos;
+        if (dir_entry.is_regular_file())
+            result_file.push_back(dir_entry);
+        else
+        {
+            result_dir.push_back(dir_entry);
+        }
+    }
+
+    sort(result_file.begin(), result_file.end());
+    sort(result_dir.begin(), result_dir.end());
+    reverse(result_file.begin(), result_file.end());
+    reverse(result_dir.begin(), result_dir.end());
+    for (auto _p : result_file)
+    {
+        dst << string(offset, ' ') << _p.filename().string() << endl;
+    }
+    for (auto _p : result_dir)
+    {
+        PrintTree(dst, _p, status, offset);
     }
 }
 
+void PrintTree(ostream& dst, const path& p)
+{
+    int offset = 0;
+    error_code err;
+    auto status = filesystem::status(p, err);
+    PrintTree(dst, p, status, offset);
+}
+
 int main() {
-    ofstream("test.txt") << "123456789"sv;
-    assert(GetFileSize("test.txt"s) == 9);
+    error_code err;
+    filesystem::remove_all("test_dir", err);
+    filesystem::create_directories("test_dir"_p / "a"_p, err);
+    filesystem::create_directories("test_dir"_p / "b"_p, err);
 
-    ofstream test2("test2.txt"s);
-    test2.seekp(1000);
-    test2 << "abc"sv;
-    test2.flush();
+    ofstream("test_dir"_p / "b"_p / "f1.txt"_p);
+    ofstream("test_dir"_p / "b"_p / "f2.txt"_p);
+    ofstream("test_dir"_p / "a"_p / "f3.txt"_p);
 
-    assert(GetFileSize("test2.txt"s) == 1003);
-    assert(GetFileSize("a file not exists"s) == string::npos);
+    ostringstream out;
+    PrintTree(cout, "test_dir"_p);
+    PrintTree(out, "test_dir"_p);
+    assert(out.str() ==
+        "test_dir\n"
+        "  b\n"
+        "    f2.txt\n"
+        "    f1.txt\n"
+        "  a\n"
+        "    f3.txt\n"sv
+    );
 }
