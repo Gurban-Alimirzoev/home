a8de8a3773cb07a7a1fb01680015997e9070b11a
Gurban Alimirzoev <95102129+Gurban-Alimirzoev@users.noreply.github.com>
2022-09-27 16:38:13 +0300

    fix

--- c++/old_example.cpp
+++ c++/old_example.cpp
@@ -1,94 +1,67 @@
-#include <algorithm>
 #include <iostream>
-#include <iomanip>
-#include <string>
+#include <list>
+#include <set>
+#include <string_view>
 #include <vector>
-#include <numeric>
+#include <span>
 
 using namespace std;
 
-class BookBase
-{
-public:
-	void SetReadUser(int id, int page)
-	{
-		if (id_and_page.size() < id + 1)
-			id_and_page.resize(id + 1);
-		if (page_and_amount.size() < page + 1)
-			page_and_amount.resize(page + 1);
+// »змените сигнатуру этой функции, чтобы она могла
+// сливать не только векторы, но и любые другие контейнеры
+template <typename InputIt1, typename InputIt2>
+void Merge(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, ostream& out) {
+    size_t i1 = 0, i2 = 0;
+    while (i1 < src1.size() && i2 < src2.size()) {
+        if (src1[i1] < src2[i2]) {
+            out << src1[i1++] << endl;
+        }
+        else {
+            out << src2[i2++] << endl;
+        }
+    }
+    while (i1 < src1.size()) {
+        out << src1[i1++] << endl;
+    }
+    while (i2 < src2.size()) {
+        out << src2[i2++] << endl;
+    }
+}
 
+template <typename T, typename S>
+void MergeSomething(T src1, S src2, ostream& out) {
+    Merge(src1, src2, out);
+}
 
-		int& id_count = page_and_amount[id_and_page[id]];
-		if (id_count != 0)
-		{
-			id_count--;
-			number_id--;
-		}
-
-		id_and_page[id] = page;
-		page_and_amount[page]++;
-		number_id++;
-	}
-
-	void CheerUser(int id)
-	{
-		if (id_and_page.size() < id)
-		{
-			result.push_back(0);
-			return;
-		}
-		int less_users = accumulate(
-			page_and_amount.begin(), 
-			next(page_and_amount.begin(), id_and_page[id]), 
-			0);
-
-		if (less_users == number_id - 1)
-		{
-			result.push_back(1);
-			return;
-		}
-		result.push_back(less_users / static_cast<double>(number_id - 1));
-	}
-
-	void PrintResult()
-	{
-		for (double less : result)
-			cout << setprecision(6) << less << endl;
-	}
-
-private:
-	vector<int> id_and_page;
-	vector<int> page_and_amount;
-	vector<double> result;
-	int number_id = 0;
-};
-
-void ParseRequsets(BookBase& bb, string line)
-{
-	if (line.substr(0, 5) == "CHEER")
-	{
-		double id = stod(line.substr(5));
-		bb.CheerUser(id);
-	}
-	else
-	{
-		line = line.substr(5);
-		double id = stod(line);
-		size_t space = line.find_first_of(' ');
-		double page = stod(line.substr(space));
-		bb.SetReadUser(id, page);
-	}
+// –еализуйте эту функцию:
+template <typename T>
+void MergeHalves(T src, ostream& out) {
+    size_t mid = (src.size() + 1) / 2;
+    // —делать Merge участка вектора от 0 до mid и от mid до конца.
+    // Ёлемент с индексом mid включаетс€ во второй диапазон.
 }
 
 int main() {
-	std::string line;
-	std::getline(cin, line);
-	int requsts_number = stoi(line);
-	BookBase bb;
-	for (int i = 0; i < requsts_number; i++)
-	{
-		getline(cin, line);
-		ParseRequsets(bb, line);
-	}
-	bb.PrintResult();
+    vector<int> v1{ 60, 70, 80, 90 };
+    vector<int> v2{ 65, 75, 85, 95 };
+    vector<int> combined{ 60, 70, 80, 90, 65, 75, 85, 95 };
+    list<double> my_list{ 0.1, 72.5, 82.11, 1e+30 };
+    string_view my_string = "ACNZ"sv;
+    set<unsigned> my_set{ 20u, 77u, 81u };
+
+    // пока функци€ MergeSomething реализована пока только дл€ векторов
+    cout << "Merging vectors:"sv << endl;
+    MergeSomething(v1, v2, cout);
+
+    cout << "Merging vector and list:"sv << endl;
+    MergeSomething(v1, my_list, cout);
+
+    cout << "Merging string and list:"sv << endl;
+    MergeSomething(my_string, my_list, cout);
+
+    cout << "Merging set and vector:"sv << endl;
+    MergeSomething(my_set, v2, cout);
+
+    cout << "Merging vector halves:"sv << endl;
+    MergeHalves(combined, cout);
 }
