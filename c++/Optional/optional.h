3d1c87972b524c2d78e1fdcfe7de8e998055b934
Gurban-Alimirzoev <95102129+Gurban-Alimirzoev@users.noreply.github.com>
2022-10-13 23:54:13 +0300

    fix

--- c++/Optional/optional.h
+++ c++/Optional/optional.h
@@ -1,82 +1,168 @@
+#pragma once
 #include <stdexcept>
 #include <utility>
 
-// Исключение этого типа должно генерироватся при обращении к пустому optional
 class BadOptionalAccess : public std::exception {
 public:
-    using exception::exception;
+	using exception::exception;
 
-    virtual const char* what() const noexcept override {
-        return "Bad optional access";
-    }
+	virtual const char* what() const noexcept override {
+		return "Bad optional access";
+	}
 };
 
 template <typename T>
 class Optional {
 public:
-    Optional() = default;
-    Optional(const T& value);
-    Optional(T&& value);
-    Optional(const Optional& other);
-    Optional(Optional&& other);
 
-    Optional& operator=(const T& value);
-    Optional& operator=(T&& rhs);
-    Optional& operator=(const Optional& rhs);
-    Optional& operator=(Optional&& rhs);
+	Optional() = default;
 
-    ~Optional()
-    {
-        delete data_;
-    }
+	Optional(const T& value) 
+	{
+		value_ = new (data_) T(value);
+		is_initialized_ = true;
+	}
 
-    bool HasValue() const
-    {
-        return is_initialized_;
-    }
+	Optional(T&& value) 
+	{
+		value_ = new (data_) T(std::move(value));
+		is_initialized_ = true;
+	}
 
-    // Операторы * и -> не должны делать никаких проверок на пустоту Optional.
-    // Эти проверки остаются на совести программиста
-    T& operator*()
-    {
-        return this->Value();
-    }
-    const T& operator*() const
-    {
-        return this->Value();
-    }
-    T* operator->()
-    {
-        return this;
-    }
-    const T* operator->() const
-    {
-        return this->Value();
-    }
+	constexpr Optional(Optional const& other) : Optional() 
+	{
+		if (other.is_initialized_) {
+			Reset();
+			value_ = new (data_) T(*other.value_);
+			is_initialized_ = true;
+		}
+	}
 
-    // Метод Value() генерирует исключение BadOptionalAccess, если Optional пуст
-    T& Value()
-    {
-        if (is_initialized_)
-            throw BadOptionalAccess();
+	constexpr Optional(Optional&& other) : Optional() 
+	{
+		if (other.is_initialized_) {
+			Reset();
+			value_ = new (data_) T(std::move(*other.value_));
+			is_initialized_ = true;
+		}
+	}
 
-        return this->Value();
-    }
-    const T& Value() const
-    {
-        if (is_initialized_)
-            throw BadOptionalAccess();
+	Optional& operator=(const T& value)
+	{
+		if (is_initialized_) {
+			*value_ = std::move(value);
+		}
+		else {
+			value_ = new ((void*)std::addressof(data_)) T(value);
+			is_initialized_ = true;
+		}
+		return *this;
+	}
 
-        return this->Value();
-    }
+	Optional& operator=(T&& rhs)
+	{
+		if (is_initialized_) {
+			*value_ = std::move(rhs);
+		}
+		else {
+			value_ = new ((void*)std::addressof(data_)) T(std::move(rhs));
+			is_initialized_ = true;
+		}
+		return *this;
+	}
 
-    void Reset()
-    {
-        
-    }
+	Optional& operator=(const Optional& rhs) 
+	{
+		if (rhs.is_initialized_) {
+			if (is_initialized_) {
+				*value_ = *rhs.value_;
+			}
+			else {
+				value_ = new (data_) T(*rhs.value_);
+
+				is_initialized_ = true;
+			}
+		}
+		else {
+			Reset();
+		}
+		return *this;
+	}
+
+
+	Optional& operator=(Optional&& rhs)
+	{
+		if (rhs.is_initialized_) {
+			if (is_initialized_) {
+				*value_ = std::move(*rhs.value_);
+			}
+			else {
+				value_ = new (data_) T(std::move(*rhs.value_));
+				is_initialized_ = true;
+			}
+		}
+		else {
+			Reset();
+		}
+		return *this;
+	}
+
+	~Optional()
+	{
+		Reset();
+	}
+
+	bool HasValue() const 
+	{
+		return is_initialized_;
+	}
+
+	T& operator*() 
+	{
+		return *value_;
+	}
+
+	const T& operator*() const 
+	{
+		return *value_;
+	}
+
+	T* operator->() 
+	{
+		return value_;
+	}
+
+	const T* operator->() const {
+		return value_;
+	}
+
+	T& Value() 
+	{
+		if (!this->is_initialized_)
+		{
+			throw BadOptionalAccess();
+		}
+		return *value_;
+	}
+
+	const T& Value() const
+	{
+		if (!this->is_initialized_) {
+			throw BadOptionalAccess();
+		}
+		return  *this->value_;
+	}
+
+	void Reset()
+	{
+		if (is_initialized_) {
+			value_->~T();
+			is_initialized_ = false;
+		}
+	}
 
 private:
-    // alignas нужен для правильного выравнивания блока памяти
-    alignas(T) char data_[sizeof(T)];
-    bool is_initialized_ = false;
+	alignas(T) char data_[sizeof(T)];
+	bool is_initialized_ = false;
+	T* value_{};
 };
