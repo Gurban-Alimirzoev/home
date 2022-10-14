f89609a88c595b9cefd00eb8c39c074156242284
Gurban-Alimirzoev <95102129+Gurban-Alimirzoev@users.noreply.github.com>
2022-10-15 00:07:49 +0300

    fix

--- c++/Vector/main.cpp
+++ c++/Vector/main.cpp
@@ -1,43 +1,72 @@
 #include "vector.h"
 
+#include <stdexcept>
+
 namespace {
 
-struct Obj {
-    Obj() {
-        ++num_default_constructed;
-    }
+    struct Obj {
+        Obj() {
+            if (default_construction_throw_countdown > 0) {
+                if (--default_construction_throw_countdown == 0) {
+                    throw std::runtime_error("Oops");
+                }
+            }
+            ++num_default_constructed;
+        }
 
-    Obj(const Obj& /*other*/) {
-        ++num_copied;
-    }
+        explicit Obj(int id)
+            : id(id)  //
+        {
+            ++num_constructed_with_id;
+        }
 
-    Obj(Obj&& /*other*/) noexcept {
-        ++num_moved;
-    }
+        Obj(const Obj& other)
+            : id(other.id)  //
+        {
+            if (other.throw_on_copy) {
+                throw std::runtime_error("Oops");
+            }
+            ++num_copied;
+        }
 
-    Obj& operator=(const Obj& other) = default;
-    Obj& operator=(Obj&& other) = default;
+        Obj(Obj&& other) noexcept
+            : id(other.id)  //
+        {
+            ++num_moved;
+        }
 
-    ~Obj() {
-        ++num_destroyed;
-    }
+        Obj& operator=(const Obj& other) = default;
+        Obj& operator=(Obj&& other) = default;
 
-    static int GetAliveObjectCount() {
-        return num_default_constructed + num_copied + num_moved - num_destroyed;
-    }
+        ~Obj() {
+            ++num_destroyed;
+            id = 0;
+        }
 
-    static void ResetCounters() {
-        num_default_constructed = 0;
-        num_copied = 0;
-        num_moved = 0;
-        num_destroyed = 0;
-    }
+        static int GetAliveObjectCount() {
+            return num_default_constructed + num_copied + num_moved + num_constructed_with_id
+                - num_destroyed;
+        }
 
-    static inline int num_default_constructed = 0;
-    static inline int num_copied = 0;
-    static inline int num_moved = 0;
-    static inline int num_destroyed = 0;
-};
+        static void ResetCounters() {
+            default_construction_throw_countdown = 0;
+            num_default_constructed = 0;
+            num_copied = 0;
+            num_moved = 0;
+            num_destroyed = 0;
+            num_constructed_with_id = 0;
+        }
+
+        bool throw_on_copy = false;
+        int id = 0;
+
+        static inline int default_construction_throw_countdown = 0;
+        static inline int num_default_constructed = 0;
+        static inline int num_constructed_with_id = 0;
+        static inline int num_copied = 0;
+        static inline int num_moved = 0;
+        static inline int num_destroyed = 0;
+    };
 
 }  // namespace
 
@@ -86,12 +115,142 @@ void Test1() {
     {
         Vector<Obj> v(SIZE);
         assert(Obj::GetAliveObjectCount() == SIZE);
+        const int old_copy_count = Obj::num_copied;
+        const int old_move_count = Obj::num_moved;
         v.Reserve(SIZE * 2);
         assert(Obj::GetAliveObjectCount() == SIZE);
+        assert(Obj::num_copied == old_copy_count);
+        assert(Obj::num_moved == old_move_count + static_cast<int>(SIZE));
     }
     assert(Obj::GetAliveObjectCount() == 0);
 }
 
+void Test2() {
+    const size_t SIZE = 100;
+    Obj::ResetCounters();
+    {
+        Obj::default_construction_throw_countdown = SIZE / 2;
+        try {
+            Vector<Obj> v(SIZE);
+            assert(false && "Exception is expected");
+        }
+        catch (const std::runtime_error&) {
+        }
+        catch (...) {
+            // Unexpected error
+            assert(false && "Unexpected exception");
+        }
+        assert(Obj::num_default_constructed == SIZE / 2 - 1);
+        assert(Obj::GetAliveObjectCount() == 0);
+    }
+    Obj::ResetCounters();
+    {
+        Vector<Obj> v(SIZE);
+        try {
+            v[SIZE / 2].throw_on_copy = true;
+            Vector<Obj> v_copy(v);
+            assert(false && "Exception is expected");
+        }
+        catch (const std::runtime_error&) {
+            assert(Obj::num_copied == SIZE / 2);
+        }
+        catch (...) {
+            // Unexpected error
+            assert(false && "Unexpected exception");
+        }
+        assert(Obj::GetAliveObjectCount() == SIZE);
+    }
+    Obj::ResetCounters();
+    {
+        Vector<Obj> v(SIZE);
+        try {
+            v[SIZE - 1].throw_on_copy = true;
+            v.Reserve(SIZE * 2);
+        }
+        catch (...) {
+            // Unexpected error
+            assert(false && "Unexpected exception");
+        }
+        assert(v.Capacity() == SIZE * 2);
+        assert(v.Size() == SIZE);
+        assert(Obj::GetAliveObjectCount() == SIZE);
+    }
+}
+
+void Test3() {
+    const size_t MEDIUM_SIZE = 100;
+    const size_t LARGE_SIZE = 250;
+    const int ID = 42;
+    {
+        Obj::ResetCounters();
+        Vector<int> v(MEDIUM_SIZE);
+        {
+            auto v_copy(std::move(v));
+
+            assert(v_copy.Size() == MEDIUM_SIZE);
+            assert(v_copy.Capacity() == MEDIUM_SIZE);
+        }
+        assert(Obj::GetAliveObjectCount() == 0);
+    }
+    {
+        Obj::ResetCounters();
+        {
+            Vector<Obj> v(MEDIUM_SIZE);
+            v[MEDIUM_SIZE / 2].id = ID;
+            assert(Obj::num_default_constructed == MEDIUM_SIZE);
+            Vector<Obj> moved_from_v(std::move(v));
+            assert(moved_from_v.Size() == MEDIUM_SIZE);
+            assert(moved_from_v[MEDIUM_SIZE / 2].id == ID);
+        }
+        assert(Obj::GetAliveObjectCount() == 0);
+
+        assert(Obj::num_moved == 0);
+        assert(Obj::num_copied == 0);
+        assert(Obj::num_default_constructed == MEDIUM_SIZE);
+    }
+    {
+        Obj::ResetCounters();
+        Vector<Obj> v_medium(MEDIUM_SIZE);
+        v_medium[MEDIUM_SIZE / 2].id = ID;
+        Vector<Obj> v_large(LARGE_SIZE);
+        v_large = v_medium;
+        assert(v_large.Size() == MEDIUM_SIZE);
+        assert(v_large.Capacity() == LARGE_SIZE);
+        assert(v_large[MEDIUM_SIZE / 2].id == ID);
+        assert(Obj::GetAliveObjectCount() == MEDIUM_SIZE + MEDIUM_SIZE);
+    }
+    {
+        Obj::ResetCounters();
+        Vector<Obj> v(MEDIUM_SIZE);
+        {
+            Vector<Obj> v_large(LARGE_SIZE);
+            v_large[LARGE_SIZE - 1].id = ID;
+            v = v_large;
+            assert(v.Size() == LARGE_SIZE);
+            assert(v_large.Capacity() == LARGE_SIZE);
+            assert(v_large[LARGE_SIZE - 1].id == ID);
+            assert(Obj::GetAliveObjectCount() == LARGE_SIZE + LARGE_SIZE);
+        }
+        assert(Obj::GetAliveObjectCount() == LARGE_SIZE);
+    }
+    assert(Obj::GetAliveObjectCount() == 0);
+    {
+        Obj::ResetCounters();
+        Vector<Obj> v(MEDIUM_SIZE);
+        v[MEDIUM_SIZE - 1].id = ID;
+        Vector<Obj> v_small(MEDIUM_SIZE / 2);
+        v_small.Reserve(MEDIUM_SIZE + 1);
+        const size_t num_copies = Obj::num_copied;
+        v_small = v;
+        assert(v_small.Size() == v.Size());
+        assert(v_small.Capacity() == MEDIUM_SIZE + 1);
+        v_small[MEDIUM_SIZE - 1].id = ID;
+        assert(Obj::num_copied - num_copies == MEDIUM_SIZE - (MEDIUM_SIZE / 2));
+    }
+}
+
 int main() {
     Test1();
+    Test2();
+    Test3();
 }
--- c++/Vector/vector.h
+++ c++/Vector/vector.h
@@ -3,6 +3,78 @@
 #include <cstdlib>
 #include <new>
 #include <utility>
+#include <memory>
+
+template <typename T>
+class RawMemory {
+public:
+    RawMemory() = default;
+
+    explicit RawMemory(size_t capacity)
+        : buffer_(Allocate(capacity))
+        , capacity_(capacity) {
+    }
+
+    ~RawMemory() {
+        Deallocate(buffer_);
+    }
+
+    RawMemory(const RawMemory&) = delete;
+    RawMemory& operator=(const RawMemory& rhs) = delete;
+
+    RawMemory(RawMemory&& other) noexcept = default;
+    RawMemory& operator=(RawMemory&& rhs) noexcept = default;
+
+    T* operator+(size_t offset) noexcept {
+        // Разрешается получать адрес ячейки памяти, следующей за последним элементом массива
+        assert(offset <= capacity_);
+        return buffer_ + offset;
+    }
+
+    const T* operator+(size_t offset) const noexcept {
+        return const_cast<RawMemory&>(*this) + offset;
+    }
+
+    const T& operator[](size_t index) const noexcept {
+        return const_cast<RawMemory&>(*this)[index];
+    }
+
+    T& operator[](size_t index) noexcept {
+        assert(index < capacity_);
+        return buffer_[index];
+    }
+
+    void Swap(RawMemory& other) noexcept {
+        std::swap(buffer_, other.buffer_);
+        std::swap(capacity_, other.capacity_);
+    }
+
+    const T* GetAddress() const noexcept {
+        return buffer_;
+    }
+
+    T* GetAddress() noexcept {
+        return buffer_;
+    }
+
+    size_t Capacity() const {
+        return capacity_;
+    }
+
+private:
+    // Выделяет сырую память под n элементов и возвращает указатель на неё
+    static T* Allocate(size_t n) {
+        return n != 0 ? static_cast<T*>(operator new(n * sizeof(T))) : nullptr;
+    }
+
+    // Освобождает сырую память, выделенную ранее по адресу buf при помощи Allocate
+    static void Deallocate(T* buf) noexcept {
+        operator delete(buf);
+    }
+
+    T* buffer_ = nullptr;
+    size_t capacity_ = 0;
+};
 
 template <typename T>
 class Vector {
@@ -10,23 +82,42 @@ public:
     Vector() = default;
 
     explicit Vector(size_t size)
-        : data_(Allocate(size))
-        , capacity_(size)
-        , size_(size)  //
+        : data_(size)
+        , size_(size)
     {
-        for (size_t i = 0; i != size; ++i) {
-            new (data_ + i) T();
-        }
+        std::uninitialized_value_construct_n(data_.GetAddress(), size);
     }
 
     Vector(const Vector& other)
-        : data_(Allocate(other.size_))
-        , capacity_(other.size_)
+        : data_(other.size_)
         , size_(other.size_)  //
     {
-        for (size_t i = 0; i != other.size_; ++i) {
-            CopyConstruct(data_ + i, other.data_[i]);
+        std::uninitialized_copy_n(other.data_.GetAddress(), size_, data_.GetAddress());
+    }
+
+    Vector(Vector&& other) noexcept = default;
+
+    Vector& operator=(const Vector& rhs) {
+        if (this != &rhs) {
+            if (rhs.size_ > data_.Capacity()) {
+                /* Применить copy-and-swap */
+            }
+            else {
+                /* Скопировать элементы из rhs, создав при необходимости новые
+                   или удалив существующие */
+            }
         }
+        return *this;
+    }
+
+    Vector& operator=(Vector&& rhs) noexcept
+    {
+        return *this;
+    }
+
+    void Swap(Vector& other) noexcept
+    {
+
     }
 
     size_t Size() const noexcept {
@@ -34,7 +125,7 @@ public:
     }
 
     size_t Capacity() const noexcept {
-        return capacity_;
+        return data_.Capacity();
     }
 
     const T& operator[](size_t index) const noexcept {
@@ -47,44 +138,30 @@ public:
     }
 
     void Reserve(size_t new_capacity) {
-        if (new_capacity <= capacity_) {
+        if (new_capacity <= data_.Capacity()) {
             return;
         }
-        T* new_data = Allocate(new_capacity);
-        for (size_t i = 0; i != size_; ++i) {
-            CopyConstruct(new_data + i, data_[i]);
+        RawMemory<T> new_data(new_capacity);
+        // Конструируем элементы в new_data, копируя их из data_
+        if constexpr (std::is_nothrow_move_constructible_v<T> || !std::is_copy_constructible_v<T>) {
+            std::uninitialized_move_n(data_.GetAddress(), size_, new_data.GetAddress());
         }
-        DestroyN(data_, size_);
-        Deallocate(data_);
-
-        data_ = new_data;
-        capacity_ = new_capacity;
+        else {
+            std::uninitialized_copy_n(data_.GetAddress(), size_, new_data.GetAddress());
+        }
+        // Разрушаем элементы в data_
+        std::destroy_n(data_.GetAddress(), size_);
+        // Избавляемся от старой сырой памяти, обменивая её на новую
+        data_.Swap(new_data);
+        // При выходе из метода старая память будет возвращена в кучу
     }
 
     ~Vector() {
-        DestroyN(data_, size_);
-        Deallocate(data_);
+        std::destroy_n(data_.GetAddress(), size_);
     }
 private:
-    T* data_ = nullptr;
-    size_t capacity_ = 0;
+    RawMemory<T> data_;
     size_t size_ = 0;
-
-    // Выделяет сырую память под n элементов и возвращает указатель на неё
-    static T* Allocate(size_t n) {
-        return n != 0 ? static_cast<T*>(operator new(n * sizeof(T))) : nullptr;
-    }
-
-    // Освобождает сырую память, выделенную ранее по адресу buf при помощи Allocate
-    static void Deallocate(T* buf) noexcept {
-        operator delete(buf);
-    }
-
-    static void DestroyN(T* buf, size_t n) noexcept {
-        for (size_t i = 0; i != n; ++i) {
-            Destroy(buf + i);
-        }
-    }
 
     // Создаёт копию объекта elem в сырой памяти по адресу buf
     static void CopyConstruct(T* buf, const T& elem) {
