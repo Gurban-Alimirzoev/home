c79d59ac1f6b98a894ee594ec46ddb2a13dd3bae
Gurban Alimirzoev <95102129+Gurban-Alimirzoev@users.noreply.github.com>
2022-11-24 13:55:47 +0300

    Update structures.cpp

--- c++/MyTable/src/structures.cpp
+++ c++/MyTable/src/structures.cpp
@@ -2,6 +2,7 @@
 
 #include <cctype>
 #include <sstream>
+#include <charconv>
 
 using namespace std;
 
@@ -12,6 +13,11 @@ const int MAX_POS_LETTER_COUNT = 3;
 static const int number_of_english_words = 26;
 
 const Position Position::NONE = {-1, -1};
+
+bool IsWord(char c)
+{
+	return c > '@' && c < '[';
+}
 
 // Реализуйте методы:
 bool Position::operator==(const Position rhs) const 
@@ -33,7 +39,7 @@ string Position::ToString() const
 {
 	if (!IsValid())
 	{
-		return;
+		return{};
 	}
 
 	string result;
@@ -53,11 +59,27 @@ Position Position::FromString(string_view str)
 	bool col = false;
 	bool row = false;
 	Position pos;
-	for (char c : str)
+
+	char first_word = str[0];
+	char second_word = str[1];
+	char thrid_word = str[2];
+
+	if (IsWord(first_word))
 	{
-		if (c > '@' && c < '[' && !row)
-		{
-			pos.col += c - '@';
-		}
+		pos.col += (first_word - '@') * number_of_english_words * number_of_english_words;
 	}
+	if (IsWord(second_word))
+	{
+		pos.col += (second_word - '@') * number_of_english_words;
+	}
+	if (IsWord(thrid_word))
+	{
+		pos.col += thrid_word - '@' - 1;
+	}
+
+	//pos.row = stoi(str.substr(3));
+	str = str.substr(3);
+	from_chars(str.begin(), str.end(), pos.row);
+
+	return pos;
 }
